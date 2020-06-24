04fb0b04aea1f413806d5a92977af39cfaa86d22
Joshua Isaac <hello.joshua@hotmail.com>
2020-06-24 19:29:39 -0400

    added plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,66 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    // pages
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    // posts
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/content/posts/`,
+      },
+    },
+    // projects
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/content/projects/`,
+      },
+    },
+    // testimonials
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `testimonials`,
+        path: `${__dirname}/src/content/testimonials/`,
+      },
+    },
+    // images
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    // image helpers
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    // mdx
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extension: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800,
+            },
+          },
+        ],
+      },
+    },
+    // SASS
+    `gatsby-plugin-sass`,
+  ],
 }
