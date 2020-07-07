444eb50b9c8f81489b9eab0d572bc46e2bf5cb53
Joshua Isaac <hello.joshua@hotmail.com>
2020-07-07 18:16:32 -0400

    fixing seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,11 @@ import React from "react"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero/Hero"
 import SEO from "../components/SEO"
-import { Helmet } from "react-helmet"
 
 export default function Home() {
   return (
     <>
-      <Helmet>
-        <title>Joshua Isaac</title>
-        <meta name="description" content="Freelance Web Developer Based in Toronto" />
-      </Helmet>
-      {/* <SEO title={`Joshua Isaac | Freelance Web Developer in Toronto`} image={} /> */}
+      <SEO title={`Joshua Isaac | Freelance Web Developer in Toronto`} />
       <Layout>
         <Hero />
       </Layout>
